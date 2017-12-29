b0ce7f87c963e8e57a7f3913823c0d4057141435
Jerol Graves <jerolgraves@gmail.com>
2017-12-29 12:54:39 -0500

    initial commit

---
+++ src/components/app/app.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './app.css';
+import BusinessList from '../businessList/businessList';
+import SearchBar from '../searchBar/searchBar';
+import Yelp from '../../util/yelp';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      businesses: []
+    };
+
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/util/yelp.js
@@ -0,0 +1,30 @@
+const apiKey = 'c3wuWwKb6mWVXawU27VruPgwo0NhLJTQSihiOS_Ko4bftvEHH5_jmaUruYuio1caKhV5JJb13gm0P434W1NOKFqU2ZpNpJNxhY_V1txXXisyar6uyef4PanpWF5CWnYx';
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+};
+
+export default Yelp;
